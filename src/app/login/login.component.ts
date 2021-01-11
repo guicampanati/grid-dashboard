e66899848b11ce71635c69f636bb02aa63ccf964
Guilherme Campanati <guicampanati@gmail.com>
2021-01-11 16:28:01 -0300

    feat: firebase auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loading = false;
+  constructor(private auth: AngularFireAuth) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async onSubmit(loginForm: NgForm) {
+    this.loading = true;
+    const { firstName, lastName, email, password } = loginForm.value;
+
+    try {
+      const res = await this.auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await res.user.updateProfile({ displayName: `${firstName} ${lastName}` });
+      loginForm.reset();
+    } catch (err) {
+      console.log(err.message);
+    }
+    this.loading = false;
   }
-
 }
