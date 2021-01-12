dfe7cbd9242ec7d3be0702aafbfc3893556f2549
Guilherme Campanati <guicampanati@gmail.com>
2021-01-12 16:40:19 -0300

    feat: user login and sign-up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,25 +10,41 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loading = false;
+  action: 'login' | 'signup' = 'login';
+  error: string;
   constructor(private auth: AngularFireAuth, private router: Router) {}
 
   ngOnInit(): void {}
 
-  async onSubmit(loginForm: NgForm) {
+  get isLogin(): boolean {
+    return this.action === 'login';
+  }
+
+  get isSignUp(): boolean {
+    return this.action === 'signup';
+  }
+
+  async onSubmit(loginForm: NgForm): Promise<void> {
     this.loading = true;
+    this.error = '';
     const { firstName, lastName, email, password } = loginForm.value;
 
+    let res;
     try {
-      const res = await this.auth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-      await res.user.updateProfile({ displayName: `${firstName} ${lastName}` });
-      loginForm.reset();
+      if (this.isSignUp) {
+        res = await this.auth.createUserWithEmailAndPassword(email, password);
+        await res.user.updateProfile({
+          displayName: `${firstName} ${lastName}`,
+        });
+        loginForm.reset();
+      } else {
+        res = await this.auth.signInWithEmailAndPassword(email, password);
+      }
+
       const uid = res.user.uid;
       this.router.navigate([`/profile/${uid}`]);
     } catch (err) {
-      console.log(err.message);
+      this.error = err.message;
     }
     this.loading = false;
   }
