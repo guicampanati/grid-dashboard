8da1d733738cf8923cfb952aee96280fbbdbd2f7
Guilherme Campanati <guicampanati@gmail.com>
2021-01-13 14:49:01 -0300

    fix: auth guard cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
-import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
-import { map } from 'rxjs/operators';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+
+const redirectLoggedInToProfile = (user) =>
+  redirectLoggedInTo(['profile', user.uid]);
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
-    canActivate: [AngularFireAuthGuard],
-    data: {
-      authGuardPipe: () =>
-        map((user) => (user ? ['profile', (user as any).uid] : true)),
-    },
+    ...canActivate(redirectLoggedInToProfile),
   },
   {
     path: 'profile/:id',
     component: ProfileComponent,
-    canActivate: [AngularFireAuthGuard],
-    data: {
-      authGuardPipe: (next) =>
-        map((user) => (!!user && next.params.id === (user as any).uid) || ['']),
-    },
+    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
 
