9a871d072cf88746430b8cb441a4d8d89229d742
Guilherme Campanati <guicampanati@gmail.com>
2021-01-14 19:24:29 -0300

    feat: storage rules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     apiKey: 'AIzaSyCwamlrH8x1cqeBF8TQ-XQhBPju5baGPyA',
     authDomain: 'grid-dashboard-cb7ab.firebaseapp.com',
     projectId: 'grid-dashboard-cb7ab',
-    storageBucket: '',
+    storageBucket: 'grid-dashboard-cb7ab.appspot.com',
     messagingSenderId: '1003044114995',
     appId: '1:1003044114995:web:2969886e019a8a4eb80946',
   },
