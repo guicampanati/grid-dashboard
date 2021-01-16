687474c5d0d93ab7d44afe83fc508c92b199b75f
Guilherme Campanati <guicampanati@gmail.com>
2021-01-16 17:08:15 -0300

    feat: admin route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,25 @@ import {
   canActivate,
   redirectLoggedInTo,
   redirectUnauthorizedTo,
+  customClaims,
 } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './users/users.component';
 
 const redirectLoggedInToProfile = (user) =>
   redirectLoggedInTo(['profile', user.uid]);
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+
+const adminOnly = () =>
+  pipe(
+    customClaims,
+    map((claims) => claims.admin === true || [''])
+  );
 
 const routes: Routes = [
   {
@@ -23,6 +33,11 @@ const routes: Routes = [
     path: 'profile/:id',
     component: ProfileComponent,
     ...canActivate(redirectUnauthorizedToLogin),
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    ...canActivate(adminOnly),
   },
 ];
 
