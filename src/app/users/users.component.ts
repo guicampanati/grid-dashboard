5d4e456294eb007b4a90e32c24a616b8fd4b1469
Guilherme Campanati <guicampanati@gmail.com>
2021-01-16 17:58:57 -0300

    feat: admin interface

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { UserProfile } from '../core/user-profile.model';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  private usersCollection: AngularFirestoreCollection<UserProfile>;
+  public users: Observable<UserProfile[]>;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.usersCollection = this.afs.collection<UserProfile>('users');
+    this.users = this.usersCollection.valueChanges();
   }
-
 }
