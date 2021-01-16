800e555172bc5c6e194d1e50df6d8232680f451f
Guilherme Campanati <guicampanati@gmail.com>
2021-01-16 18:25:27 -0300

    feat: admin profile access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
-import {
-  AngularFireAuthGuard,
-  canActivate,
-  customClaims,
-  redirectUnauthorizedTo,
-} from '@angular/fire/auth-guard';
+import { canActivate, customClaims } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -12,12 +7,21 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UsersComponent } from './users/users.component';
 
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
-
 const adminOnly = () =>
   pipe(
     customClaims,
     map((claims) => claims.admin === true || [''])
+  );
+
+const allowOnlySelfOrAdmin = (next) =>
+  pipe(
+    customClaims,
+    map((claims) => {
+      if (claims.length === 0) {
+        return [''];
+      }
+      return next.params.id === claims.user_id || claims.admin;
+    })
   );
 
 const redirectLoggedInToProfileOrUsers = () =>
@@ -41,13 +45,12 @@ const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectLoggedInToProfileOrUsers },
+    ...canActivate(redirectLoggedInToProfileOrUsers),
   },
   {
     path: 'profile/:id',
     component: ProfileComponent,
-    ...canActivate(redirectUnauthorizedToLogin),
+    ...canActivate(allowOnlySelfOrAdmin),
   },
   {
     path: 'users',
