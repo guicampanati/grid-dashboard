a0a6642e228f816b92a3de3d6d82828d98baa8cd
Guilherme Campanati <guicampanati@gmail.com>
2021-01-22 17:23:04 -0300

    config: deploy configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCwamlrH8x1cqeBF8TQ-XQhBPju5baGPyA',
+    authDomain: 'grid-dashboard-cb7ab.firebaseapp.com',
+    projectId: 'grid-dashboard-cb7ab',
+    storageBucket: 'grid-dashboard-cb7ab.appspot.com',
+    messagingSenderId: '1003044114995',
+    appId: '1:1003044114995:web:2969886e019a8a4eb80946',
+  },
 };
