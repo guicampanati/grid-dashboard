1e321fbe8856e84cd8889e9c4c64e04a2141e69a
Guilherme Campanati <guicampanati@gmail.com>
2021-01-22 16:57:51 -0300

    feat: cloud function auth trigger

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,44 @@
-/* eslint-disable object-curly-spacing */
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+const APP_NAME = 'grid-dashboard';
+
+/**
+ * Send email to new users
+ * @param {string} email The user email.
+ * @param {string} displayName The user name.
+ * @return {null}
+ */
+async function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <email@email.com>`,
+    to: email,
+  };
+
+  mailOptions.subject = `Welcome to ${APP_NAME}`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to the team`;
+
+  await mailTransport.sendMail(mailOptions);
+
+  console.log('New Welcome email sent to: ', email);
+
+  return null;
+}
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+  const email = user.email;
+  const displayName = user.displayName;
+
+  return sendWelcomeEmail(email, displayName);
 });
